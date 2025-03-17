eb184a2c1db1a9bc16a2c4cdf9246e25fbe3ed16
Dariana Sanguino Cuello <stroncoso@uninorte.edu.co>
2025-03-17 08:37:31 -0500

    Added Class Person

---
+++ src/core/Person.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author sdariana
+ */
+public class Person {
+    String name;
+    int age;
+    int id;
+
+    public Person(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    
+}
